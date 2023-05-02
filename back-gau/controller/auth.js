0f8119c23c9f178bf2064e3096e41ac9dcf939a8
Zodiako <cristianjvz98@gmail.com>
2023-05-02 14:03:12 -0500

    feature: registrar y logear desde back con hash

---
+++ back-gau/controller/auth.js
@@ -0,0 +1,53 @@
+
+const { firmarToken } = require('../utils/handleJwt')
+const { profesoresModel } = require('../models')
+const { encriptar, comparar } = require('../utils/handlePassword')
+const { matchedData } = require('express-validator')
+const { handleHttpError } = require('../utils/handleError')
+
+const registrarCtrl = async (req, res) => {
+    try {
+        req = matchedData(req)
+        const password = await encriptar(req.password)
+        const body = { ...req, password }
+        const dataProfesor = await profesoresModel.create(body)
+        dataProfesor.set('password', undefined, { strict: false })
+        const data = {
+            token: await firmarToken(dataProfesor),
+            user: dataProfesor
+        }
+        res.send({ data })
+    } catch (error) {
+        handleHttpError(res, "Error registrarProfesor")
+    }
+
+}
+
+const loginCtrl = async (req, res) => {
+    try {
+        req = matchedData(req)
+        const profesor = await profesoresModel.findOne({ numeroIdentificacion: req.numeroIdentificacion }).select("password")
+        if (!profesor) {
+            handleHttpError(res, "Profesor no existe", 404)
+            return
+        }
+        const hashPassword = profesor.get('password')
+        const verificar = await comparar(req.password, hashPassword)
+
+        if (!verificar) {
+            handleHttpError(res, "Error en contraseña", 401)
+            return
+        }
+        profesor.set('password', undefined, { strict: false })
+        const data = {
+            token: firmarToken(profesor),
+            profesor
+        }
+
+        res.send({ data })
+    } catch (error) {
+        handleHttpError(res, "Error en controladorLogin", 404)
+    }
+}
+
+module.exports = { registrarCtrl, loginCtrl }
--- back-gau/routes/auth.js
+++ back-gau/routes/auth.js
@@ -1,24 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { validacionLogin, validacionRegistrarProfesor } = require('../validators/auth')
-const { firmarToken } = require('../utils/handleJwt')
-const { profesoresModel } = require('../models')
-const { encriptar } = require('../utils/handlePassword')
-const { matchedData } = require('express-validator')
+const { registrarCtrl, loginCtrl } = require('../controller/auth')
 
 
 
-router.post("/registrar", validacionRegistrarProfesor, async (req, res) => {
-    req = matchedData(req)
-    const contrasennaHashd = await encriptar(req.password)
-    const body = { ...req, contrasennaHashd }
-    const dataProfesor = await profesoresModel.create(body)
-    dataProfesor.set('password', undefined, { strict: false })
-    const data = {
-        token: await firmarToken(dataProfesor),
-        user: dataProfesor
-    }
-    res.send({ data: data })
-})
+router.post("/registrar", validacionRegistrarProfesor, registrarCtrl)
+router.post("/login", validacionLogin, loginCtrl)
 
 module.exports = router
