4e9d7f6f4e55864096a87825acd35cddb0e13225
Pancho Polanco <cristian.pancho.cami@gmail.com>
2023-05-04 15:30:42 -0500

    feature modificar: verificacionn login

--- front-gau/src/views/ListaEstudiantes.vue
+++ front-gau/src/views/ListaEstudiantes.vue
@@ -43,7 +43,7 @@
                       </div>
                       <div class="mb-3 n">
                         <label for="exampleInputPassword1" class="form-label">Nombre Completo</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="nombre">
+            <input type="password" class="form-control" v-model="nombre_completo" id="exampleInputPassword1" placeholder="nombre">
                       </div>
                       <div class="mb-3 n">
                         <label for="exampleInputPassword1" class="form-label">Correo</label>
@@ -243,6 +243,7 @@
       return {
         showModal: false,
         posts: [],
+        nombre_completo: '',
       };
     },
     methods: {
