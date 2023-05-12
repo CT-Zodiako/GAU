2069a276527eb4a1e19627a0740a9df7e4f6d724
Pancho Polanco <cristian.pancho.cami@gmail.com>
2023-05-12 17:09:37 -0500

    eliminar un estudiante correcto

--- front-gau/src/services/estudiantesServicio.js
+++ front-gau/src/services/estudiantesServicio.js
@@ -31,7 +31,7 @@ export default class estudianteServicio {
     }
   }
 
-  async editarEstudiantes( nombreCompleto, numeroIdentificacion) {
+  async editarEstudiantes( _id, nombreCompleto, numeroIdentificacion) {
     try {
       const response = await axios.put(
         api_endpoint + "estudiantes",{
@@ -47,12 +47,13 @@ export default class estudianteServicio {
     }
   }
 
-  async eliminarEstudiantes(_id) {
+  async eliminarEstudiantes(idEstudiante) {
     try {
       await axios.delete(
-        api_endpoint + "estudiantes"+_id,
-        );
-      return this.getEstudiantes();
+        api_endpoint + `estudiantes/${idEstudiante}`,
+      );
+      
+      return this.getEstudiantes;
     } catch (err) {
       console.error(err);
       // AuthControl.verificarStatusCode(err);
--- front-gau/src/views/ListaEstudiantes.vue
+++ front-gau/src/views/ListaEstudiantes.vue
@@ -104,7 +104,7 @@
             <tbody>
               <tr v-for="post in posts" :key="post._id" :src="post.thumbnailURL" :alt="post.title">
                 <td>
-                  {{ post.nombreCompleto }}
+                  {{ post._id }}
                 </td>
                 <td>
                   <div>
@@ -121,7 +121,7 @@
                 </td>
                 <td class="opciones">
                   <div>
-                    <button type="button" class="btn btn-warning" @click="editar = true">
+                    <button type="button" class="btn btn-warning" @click="editar=(post._id)" style="margin-right: 15px;">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -326,10 +326,10 @@ export default {
         await this.estudianteServicio.editarEstudiantes(this._id, this.nombreCompleto, this.numeroIdentificacion);
     },
 
-    deletePost: async function () {
+    deletePost: async function (idEstudiante) {
         this.estudianteServicio = new estudianteServicio();
-        await this.estudianteServicio.eliminarEstudiantes(this._id);
-    }
+        await this.estudianteServicio.eliminarEstudiantes(idEstudiante);
+    },
   },
 
   created() {
